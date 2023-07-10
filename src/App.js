9237c32aecaf71482046afa162d5480eec51e1de
Dustin Kelley <kelleydustin2018@gmail.com>
2023-07-09 20:42:14 -0500

    added about component and started skills component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+   <>
+    <Header />
+
+    <main className='main'>
+      <Home />
+      <About />
+      <Skills />
+    </main>
+   </>
   );
 }
 
---
+++ src/components/home/Social.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+        <a href='https://github.com/DustinKelley?tab=repositories'  rel="noreferrer" className='home__social-icon' target='_blank'>
+            <i className='uil uil-github-alt'></i>
+        </a>
+
+        <a href='https://github.com/DustinKelley?tab=repositories'  rel="noreferrer" className='home__social-icon' target='_blank'>
+            <i className='uil uil-github-alt'></i>
+        </a>
+
+        <a href='https://github.com/DustinKelley?tab=repositories'  rel="noreferrer" className='home__social-icon' target='_blank'>
+            <i className='uil uil-github-alt'></i>
+        </a>
+
+       
+    
+    </div>
+  )
+}
+
+export default Social;
