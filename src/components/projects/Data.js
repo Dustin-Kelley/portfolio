40d633f02cfbff690935e545737689e19f8dae63
Dustin Kelley <kelleydustin2018@gmail.com>
2023-07-12 20:24:33 -0500

    corrected css issues

---
+++ src/components/projects/Data.js
@@ -0,0 +1,38 @@
+import Work1 from '../../assets/assets/work1.jpg';
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title: 'crwn-clothing',
+        category: 'web app',
+    },
+    {
+        id: 1,
+        image: Work1,
+        title: 'crwn-clothing',
+        category: 'mobiler',
+    },
+    {
+        id: 1,
+        image: Work1,
+        title: 'crwn-clothing',
+        category: 'E-commerce',
+    },
+
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'web app',
+    },
+    {
+        name: 'mobile',
+    },
+    {
+        name: 'E-commerce',
+    },
+];
