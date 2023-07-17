da97039a44090c111df371349711eb68a1f9985b
Dustin Kelley <kelleydustin2018@gmail.com>
2023-07-16 20:32:36 -0500

    finished contact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './Contact.css';
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_hzwulil', 'template_ydslx45', form.current, '6s-WOAsRAbbCzA340')
+       e.target.reset()
+    };
   return (
     <section className='contact section' id='contact'>
         <h2 className='section__title'>Get in touch</h2>
@@ -49,7 +59,7 @@ const Contact = () => {
         <div className='contact__content'>
             <h3 className='contact__title'>Send me your project</h3>
 
-            <form className='contact__form'>
+            <form ref={form} onSubmit={sendEmail} className='contact__form'>
 
             <div className='contact__form-div'>
                     <label className='contact__form-tag'>Name</label>
@@ -69,7 +79,7 @@ const Contact = () => {
 
                 </div>
 
-                <div className='contact__form-div'>
+                <div className='contact__form-div contact__form-area'>
                     <label className='contact__form-tag'>Project</label>
                    <textarea name='project' cols='30' rows='10' 
                    className='contact__form-input' 
